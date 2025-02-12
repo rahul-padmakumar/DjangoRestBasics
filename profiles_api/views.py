a7312f73a323cee94ba12d7ed4cb1a75bd1c522e
rahulp <rahulp5875@gmail.com>
2025-02-12 17:51:02 +0530

    Learn about ApiViews.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import HelloSerializer
 
 # Create your views here.
+class HelloApiView(APIView):
+    def get(self, req):
+        return Response({"method": "GET"})
+    def post(self, req):
+        serializer =  HelloSerializer(data=req.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            return Response({"method": "POST", "value": name})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUESTS)
+        
+    def put(self, req, pk=None):
+        return Response({"method": "PUT"})
+    
+    def patch(self, req, pk=None):
+        return Response({"method": "PATCH"})
+    
+    def delete(self, req, pk=None):
+        return Response({"method": "DELETE"})
