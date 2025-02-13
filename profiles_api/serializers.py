c3674607fabc9a8545022bdd20d7050ea98d6612
rahulp <rahulp5875@gmail.com>
2025-02-13 21:46:38 +0530

    Mask password while creating and updating user profile.

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -18,3 +18,17 @@ class ProfileSerializer(serializers.ModelSerializer):
             }
         }
 
+
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
+        return user
+    
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+        return super().update(instance, validated_data)
